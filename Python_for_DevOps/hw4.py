babcfec0c7108c3c61e7d78bc1bb5e2bf93c9144
dimivanchenko <dim.ivanchenko@gmail.com>
2022-02-14 04:32:10 +0200

    Corrections in module 4

--- Python_for_DevOps/hw4.py
+++ Python_for_DevOps/hw4.py
@@ -1,5 +1,18 @@
 import rpm
 import sys
+import rpmfile
+
+# rpmfile module
+
+filename = sys.argv[1]
+
+with rpmfile.open(filename) as rpmf:
+    print("name of rpm - ", rpmf.headers.get('name').decode('ascii'))
+    print("release - ", rpmf.headers.get('release').decode('ascii'))
+    print("version - ", rpmf.headers.get('version').decode('ascii'))
+  #  print(rpmf.headers.keys())
+
+# rpm module
 
 filename = sys.argv[1]
 rpm_file = open(filename)
@@ -7,11 +20,9 @@ ts = rpm.TransactionSet()
 package = ts.hdrFromFdno(rpm_file)
 
 for index in package:
-    print "VERSION........", package[rpm.RPMTAG_VERSION]
-    print "RELEASE........", package[rpm.RPMTAG_RELEASE]
-    print "PACKAGER.......", package[rpm.RPMTAG_PACKAGER]
-
-print (str(package[rpm.RPMTAG_VERSION])[:1]+".rel"+str(package[rpm.RPMTAG_RELEASE][:1])+"."+str(package[rpm.RPMTAG_PACKAGER])[:6])
+    print ("VERSION........", package[rpm.RPMTAG_VERSION].decode('ascii'))
+    print ("RELEASE........", package[rpm.RPMTAG_RELEASE].decode('ascii'))
+    print ("PACKAGER.......", package[rpm.RPMTAG_PACKAGER].decode('ascii'))
 
 rpm_file.close()
 
